c897d41ebea16c1767aed7a85116ac2f8f955c80
voxelstack <voxelstack@gmail.com>
2023-06-18 15:37:57 -0300

    Setup wasm.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	import { greet } from 'dotatlas';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		greet();
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), wasm(), topLevelAwait()],
+	server: {
+		fs: {
+			allow: ['./dotatlas']
+		}
+	}
 });
