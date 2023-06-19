c83e2c960e97625a7295098d60302f1656b7e842
voxelstack <voxelstack@gmail.com>
2023-06-19 16:09:49 -0300

    Add wasm tests.

--- src/dotatlas/lib.rs
+++ src/dotatlas/lib.rs
@@ -39,3 +39,14 @@ pub fn attach(canvas: OffscreenCanvas) {
 
     context.stroke();
 }
+
+#[cfg(test)]
+mod tests {
+    use wasm_bindgen_test::*;
+    wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+    #[wasm_bindgen_test]
+    fn pass() {
+        assert_eq!(1, 1);
+    }
+}
